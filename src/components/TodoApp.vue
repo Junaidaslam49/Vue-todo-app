5f86f02a22f8bdb346bfa54c1f355cfe574c0330
Junaid Aslam <junaid.aslam@gsquad.io>
2021-05-26 18:24:12 +0500

    Install fontawsome and bootstrap in project

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>HelloWorld</div>
+  <div class="container">
+    <div class="todo-card">
+      <h1 class="text-center mt-5">Vue Todo App</h1>
+      <div class="w-50 mx-auto">
+        <!-- Input,s -->
+        <input type="text" placeholder="Enter a task" class="form-control mb-4">
+        <input type="text" placeholder="Description" class="input-lg form-control mb-4">
+        <div class="text-center">
+          <button class="btn btn-primary btn-lg">Submit</button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tasks Table -->
+    <div class="todo-card">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Task</th>
+            <th scope="col">Description</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Trash</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Steal bnana,s from store</td>
+            <td>To-do</td>
+            <td>
+              <div class="text-center">
+                <span class="fa fa-pen"></span>
+              </div>
+            </td>
+            <td>
+              <div class="text-center">
+                <span class="fa fa-trash"></span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,9 +49,30 @@ export default {
   name: 'TodoList',
   props: {
     msg: String
+  },
+
+  data() {
+    return {
+      tasks: [
+        {
+          name: 'Banana,s',
+          description: 'Steal banana,s from store'
+        },
+        {
+          name: 'Choclate',
+          description: 'Eat 1kg choclate after meal'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style scoped>
+  .todo-card{
+    margin: 25px auto;
+    border-radius: 6px;
+    background-color: #FFFFFF;
+    box-shadow: 0 0 14px 0 rgb(0 0 0 / 7%);
+  }
 </style>
